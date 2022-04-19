f16b5272ea43251aa42260694c4aa1c7564b0a64
Harold Calayan <haroldcalayan@gmail.com>
2022-04-19 11:31:33 +0800

    :sparkles: Create extensions and constant files

---
+++ app/src/main/java/com/haroldcalayan/scanmecalculator/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.haroldcalayan.scanmecalculator.util
+
+object Constants {
+
+    const val ONE_SECOND = 1000L
+    const val ONE_MINUTE = 60 * ONE_SECOND
+
+    const val SPLASH_SCREEN_LIFE = 3 * ONE_SECOND
+
+}
